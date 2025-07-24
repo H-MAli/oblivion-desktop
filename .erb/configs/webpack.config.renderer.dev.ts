dea9d3d328ed09ed7cc5dc4872b67b5ea38006b0
Mohammad Ali Haghshenas <haghshenas_m_a@yahoo.com>
2025-07-25 00:34:11 +0330

    chore: update package.json with new dependencies, scripts, and build configurations

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -53,7 +53,10 @@ const configuration: webpack.Configuration = {
             react: path.resolve(__dirname, '../../node_modules/react'),
             'react-dom': path.resolve(__dirname, '../../node_modules/react-dom'),
             'react/jsx-runtime': path.resolve(__dirname, '../../node_modules/react/jsx-runtime'),
-            'react/jsx-dev-runtime': path.resolve(__dirname, '../../node_modules/react/jsx-dev-runtime')
+            'react/jsx-dev-runtime': path.resolve(
+                __dirname,
+                '../../node_modules/react/jsx-dev-runtime'
+            )
         }
     },
 
